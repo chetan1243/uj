9b39178529f0849f822e8a3f824359e8437ba7d6
chetan1243 <45307042+chetan1243@users.noreply.github.com>
2018-11-28 18:25:16 +0530

    Add files via upload

---
+++ commands/Claw/DM.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  let user = message.mentions.members.first();
+    let msg = args.slice(1).join(" ");
+    if(!user) return message.reply("Mention A User To send a Message");
+    if(!message) return message.reply("Provide Me Some Message To Send To The Mentioned User!!")
+    message.delete();
+    user.send(msg);
+    message.channel.send("Done!!").then(mesg => {
+    mesg.react("✅")
+    });
+}
+module.exports.command = {
+        name: 'DM',
+        permission: "ADMINISTRATOR",
+        description: "DM A USER",
+        usage: `FOR DIRECT MESSAGE`,
+        category: "CLAW",
+        enabled: true
+    }
+
