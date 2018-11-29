560fa62eae6c703fc463cd105d55e32354670c9a
chetan1243 <45307042+chetan1243@users.noreply.github.com>
2018-11-29 07:03:58 +0530

    Update DM.js

--- commands/Claw/DM.js
+++ commands/Claw/DM.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
 const botconfig = require("../../botconfig.json");
 module.exports.run = async (bot, message, args) => {
-if(message.author.id !== botconfig.owner) return;
-  let user = message.mentions.members.first();
+if(message.author.id == botconfig.owner) return;
+  { let user = message.mentions.members.first();
     let msg = args.slice(1).join(" ");
     if(!user) return message.reply("Mention A User To send a Message");
     if(!message) return message.reply("Provide Me Some Message To Send To The Mentioned User!!")
     message.delete();
-    user.send(msg);
+    user.send(msg);}
     message.channel.send("Done!!").then(mesg => {
     mesg.react("✅")
     });
