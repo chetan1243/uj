7edb66ce5d0f78d9f6509fd42ca5a584a1ee2180
chetan1243 <45307042+chetan1243@users.noreply.github.com>
2019-01-25 21:16:55 +0530

    Update clanmember.js

--- commands/Claw/clanmember.js
+++ commands/Claw/clanmember.js
@@ -9,9 +9,9 @@ module.exports.run = async(bot, message) => {
     .setColor("#00ff00")
     .setThumbnail(bicon)
     .addField("CLAN FOUNDER-","CLAW | ALEX THE HAWK")
-    .addField("CLAN ADMINS","1.CLAW | GHOST \n 2.CLAW | VISHESH")
-    .addField("CLAN MODRETORS","1.CLAW | BISWAJIT \n 2.CLAW | THUNDERBOLT \n 3.CLAW | CHAITU \n 4.CLAW | JP \n 5.CLAW | MR_EVIL")
-    .addField("CLAN MEMBERS","1.CLAW | BUNNY \n 2.CLAW | ROHAN \n 3.CLAW | ANCIENT \n 4.CLAW | ANKUR \n 5.CLAW | ASSASSIN \n  6.CLAW | AKASH \n 7.CLAW | KUNAL \n 8.CLAW | ELECTRON \n 9.CLAW | ZAP \n 10.CLAW | ARYAN");
+    .addField("CLAN ADMINS","1.CLAW | GHOST \n 2.CLAW | VISHESH \n 3.CLAW | BISWAJIT")
+    .addField("CLAN MODRETORS","1.CLAW | THUNDERBOLT \n 2.CLAW | CHAITU \n 3.CLAW | JP \n 4.CLAW | MR_EVIL")
+    .addField("CLAN MEMBERS","1.CLAW | BUNNY \n 2.CLAW | ROHAN \n 3.CLAW | ANCIENT \n 4.CLAW | ANGRY \n 5.CLAW | ASSASSIN \n  6.CLAW | Ashwin \n 7.CLAW | KUNAL \n 8.CLAW | Manish \n 9.CLAW | ZAP \n 10.CLAW | ARYAN \n 11.CLAW | Anubhav \n 12.CLAW | GUCCIMAN \n 13.CLAW | GODSPEED");
 
     message.channel.send(botembed);
 
